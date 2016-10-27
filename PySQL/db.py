ba14362f528ca2eeca800b4cc0f95f85e5092e50
Daniel Monteagudo <dmonte3210@gmail.com>
2016-10-26 23:12:49 -0400

    added scrambling functions

--- PySQL/db.py
+++ PySQL/db.py
@@ -1,18 +1,59 @@
 import sqlite3
+import collections
+import random
+from Crypto.Cipher import AES
 
 database = None
 c = None
 location = None
 initError = "You have to initialize the database first. Run db.init(*filename*)"
 
+def scramble(filename):
+    datafile = open("data.db", "r")
+    blub = datafile.read()
+    datafile.close()
+    
+    key = b''
+    for i in range(0,16):
+        key+=str(random.randint(0,9))
+        
+    print key
+        
+    cipher = AES.new(key)
+        
+    blub = cipher.encrypt(blub)
+    
+    datafile = open("data.db", "w")
+    datafile.write(blub)
+    datafile.close()
+    
+def unscramble(filename):
+    datafile = open(filename, 'r')
+    blub = datafile.read()
+    datafile.close()
+    
+    key = raw_input("Key: ")
+    cipher = AES.new(key)
+    
+    blub = cipher.decrypt(blub)
+    
+    datafile = open(filename, 'w')
+    datafile.write(blub)
+    datafile.close()
+        
 #Also important
-def init(filename):
+def init(filename="data.db"):
     global database
     global c
     global location
     location = filename
     database = sqlite3.connect(filename)
     c = database.cursor()
+    try:
+        c.execute("PRAGMA schema_version")
+    except sqlite3.DatabaseError:
+        descramble(filename)
+        
 
 def tables():
     if c != None:
@@ -25,11 +66,26 @@ def tables():
 def columns(tablename):
     if c != None:
         c.execute("PRAGMA table_info(%s)" % tablename)
-        stringdict = {}
+        stringdict = collections.OrderedDict()
         for tuplet in c.fetchall():
             stringdict[str(tuplet[1])] = str(tuplet[2])
         return stringdict
 
+def select(tablename, rows, where=""):
+    if c != None:
+        commandString = "SELECT "
+        for row in rows:
+            commandString += "%s, " % row
+        commandString = commandString[:len(commandString)-2]
+        commandString += " FROM %s" % tablename
+        if where != "":
+            commandString+=" WHERE %s" % where
+        print commandString
+        c.execute(commandString)
+        return c.fetchall()
+    else:
+        print initError    
+    
 def create(tablename, rows):
     global c
     if c != None:
@@ -38,7 +94,6 @@ def create(tablename, rows):
             for row in rows:
                 commandString += "%s %s, " % (row, rows[row])
             commandString = commandString[:len(commandString)-2] + ")"
-            print commandString
             c.execute(commandString)
         else:
             print "Table \"%s\" already exists" % tablename
@@ -55,7 +110,7 @@ def add(tablename, items):
                 commandString += str(item) + ', '
             except ValueError:
                 commandString += '"'    
-                commandString += str(item) + '", '                
+                commandString += str(item) + '", '
         commandString = commandString[:len(commandString)-2] + ")"
         c.execute(commandString)
     else:
